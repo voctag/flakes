681c0564120f2b34702d5ed5ba3c45beef51a365
Manuel Schiner <maschiner@maschiner.com>
2018-09-07 15:04:52 +0200

    update ruby version / update gems / bump version

--- spec/flakes_spec.rb
+++ spec/flakes_spec.rb
@@ -1,3 +1,15 @@
+class Testing < Flake
+  attr_accessor :input
+
+  def execute
+    with_valid_params do
+      success(input)
+    end
+  end
+end
+
 describe Flake do
-  pending "write it"
+  it "retuns success input" do
+    expect(Testing.execute(input: "success")).to eql("success")
+  end
 end
